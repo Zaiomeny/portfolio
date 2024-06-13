34ccfedca5f9e3d68a8763087aed07571fd51489
Zaiomeny <80757833+Zaiomeny@users.noreply.github.com>
2024-06-13 15:53:19 +0300

    'change the part of code which get the id of the element in tab-menu'

--- js/myscript.js
+++ js/myscript.js
@@ -1,19 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
     const links = document.querySelectorAll('.menu-item');
-    const tab_menus = document.querySelectorAll('.tab-menu a');
+    const tab_menus = document.querySelectorAll('.tab-menu span');
 
     const removeActiveClass = (elements) => {
         elements.forEach((el)=>{
             el.classList.remove("active")
         })
     }
-const activeLink = (elements, targeted = false) => {
+
+    const activeLink = (elements, targeted = false) => {
         for (let element of elements) {
             element.addEventListener('click', (e) => {
                 removeActiveClass(elements)
                 e.target.classList.add('active')
                 if (targeted) {
-                    const targetId = e.target.getAttribute('href')
+                    const targetId = e.target.dataset.target
                     const tab_contents = document.querySelectorAll('.tab-content div')
                     removeActiveClass(tab_contents)
                     document.querySelector(targetId).classList.add('active')
@@ -23,4 +24,5 @@ const activeLink = (elements, targeted = false) => {
     }
     activeLink(links)
     activeLink(tab_menus, true)
+
 })
